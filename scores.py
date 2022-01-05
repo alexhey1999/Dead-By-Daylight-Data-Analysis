be6dabf8d94b1444e617e04b714f02663b20ce13
Alex Hey <alex.hey@tclarke.co.uk>
2022-01-05 07:54:06 +0000

    adjusted values on offering detection

--- scores.py
+++ scores.py
@@ -2,34 +2,35 @@ import cv2
 import numpy as np
 
 
-def adjustScreenSizeOfferings(Screen):
-    widthStartCut = 370
-    widthEndCut = 1495
+def adjustScreenSizeScores(Screen):
+    widthStartCut = 430
+    widthEndCut = 1445
     hightStartCut = 310
-    hightEndCut = 300
+    hightEndCut = 400
 
     # print(Screen.shape)
     Screen = Screen[0+hightStartCut:Screen.shape[0]-hightEndCut, 0+widthStartCut:Screen.shape[1]-widthEndCut]
     # cv2.imshow("Screen", Screen)
     return Screen
 
-def calculateOfferings(offeringsList, location, Screen):
-    offerings = {}
-    size = 45
-    cropBorder = 7
-    threshold = 0.92
+def calculateScores(itemList, location, Screen):
+    items = {}
+    size = 37
+    cropBorder = 0
+    threshold = 0.81
+    
 
     firstrun = False
 
-    for item in offeringsList:
+    for item in itemList:
         icon = cv2.imread(location+item)
         icon = cv2.resize(icon, (size, size),interpolation=cv2.INTER_AREA)
         icon = icon[cropBorder:size-cropBorder , cropBorder:size-cropBorder]
+
         if firstrun:
-                cv2.imshow("Screen", Screen)
-                cv2.imshow("Icon", icon)
-                firstrun = False
-        
+            cv2.imshow("Screen", Screen)
+            cv2.imshow("Icon", icon)
+            firstrun = False
 
         result = cv2.matchTemplate(Screen, icon, cv2.TM_CCORR_NORMED)
         yloc, xloc = np.where(result >= threshold)
@@ -43,9 +44,9 @@ def calculateOfferings(offeringsList, location, Screen):
         
         #Perk Count
         if len(rectangles) > 0:
-            offerings[item] = len(rectangles)
-            
+            items[item] = len(rectangles)
+           
             # cv2.imshow("Item", Screen)
             # print(f'{perk} : {len(rectangles)}')
     # print(perks)
-    return offerings
+    return items
