fb841fb74e4a5e20d92164f25030aa3a37c600e8
Alex Hey <alexhey1999@gmail.com>
2022-01-08 01:21:58 +0000

    Removed entropy calculations

--- testing.py
+++ testing.py
@@ -1,6 +1,10 @@
+from math import nan
 import cv2
 import numpy as np
 import PIL.Image as Image
+import random
+from numpy.core.numeric import NaN
+from scipy.stats import entropy
 
 def testingScreenAdjust(Screen):
     print(Screen.shape)
@@ -34,12 +38,17 @@ def testingScreenAdjust(Screen):
 
 def testingBlackAndWhite(perkList,location,Screen):
     perks = {}
-    size = 47
-    threshold = 0.60
-    cropBorder = 4
+    size = 45
+    thresholdOriginal = 0.65
+    cropBorder = 0
+
+    scalar = 0.25751
+
+    minEnt, maxEnt, aveEnt = 1.5887406794040793, 2.8644528562126528, 2.3302580610210795
 
     firstrun = True
     #convert screen to black and white
+    entropyList = []
 
     for perk in perkList:
         icon = cv2.imread(location+perk)
@@ -47,48 +56,77 @@ def testingBlackAndWhite(perkList,location,Screen):
         icon = cv2.resize(icon, (size, size),interpolation=cv2.INTER_AREA )
 
 
-
-        # upper_white = np.array([255, 255, 255])
-        # lower_white = np.array([90, 90, 90])
-
-        # mask = cv2.inRange(icon, lower_white, upper_white)
-
-        # WhiteBackground = np.zeros((icon.shape[0], icon.shape[1], 3), dtype=np.uint8)
-        # WhiteBackground[:,:,:] = (255,255,255)
-
-        # BlackBackground = np.zeros((icon.shape[0], icon.shape[1], 3), dtype=np.uint8)
-        # BlackBackground[:,:,:] = (0,0,0)
-
-
-
-        # result = cv2.bitwise_and(WhiteBackground,WhiteBackground,BlackBackground ,mask = mask)
-
-        # icon = cv2.blur(icon,(3,3))
-
         icon = icon[cropBorder:size-cropBorder , cropBorder:size-cropBorder]
 
         # cv2.imshow(perk, icon)
 
+        ent = entropy(icon)
+
+        f = []
+        for i in ent:
+            if not(np.isnan(i[0])):
+                f.append(i) 
+        entropyList.append(np.average(f))
+
+        # threshold = np.average(f) * scalar
+        issuePerks = ["iconPerks_BoonCircleOfHealing.png","iconPerks_BoonExponential","iconPerks_trailOfTorment.png","iconPerks_dragonsGrip.png"]
+
+        if perk in issuePerks:
+            threshold = 0.49
+        else:
+            threshold = thresholdOriginal
+
         result = cv2.matchTemplate(Screen, icon, cv2.TM_CCOEFF_NORMED)
         yloc, xloc = np.where(result >= threshold)
+
+
         rectangles = []
 
-        if perk == "iconPerks_quickAndQuiet.png":
-            cv2.imshow("Screen", Screen)
+        testingPerkList = ["iconPerks_DeadHard.png","iconPerks_BoonCircleOfHealing.png","iconPerks_bond.png"]
+
+        if perk in testingPerkList:
+            # cv2.imshow("Screen", Screen)
+            # print(icon)
+
+            #Double Size of Icon
+            icon = cv2.resize(icon, (size*2, size*2),interpolation=cv2.INTER_AREA )
             cv2.imshow("Icon", icon)
-            firstrun = False
 
         for (x, y) in zip(xloc, yloc):
-            rectangles.append([int(x-cropBorder), int(y-cropBorder), size,size])
-            rectangles.append([int(x-cropBorder), int(y-cropBorder), size,size])
+            rectangles.append([int(x), int(y), size-cropBorder,size-cropBorder])
+            rectangles.append([int(x), int(y), size-cropBorder,size-cropBorder])
 
         rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+        
+        color = tuple(list(np.random.choice(range(256), size=3)))
+
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        for (x, y, w, h) in rectangles:
+            # cv2.putText()
+
+            cv2.putText(Screen, perk.split("_")[1].split(".")[0], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, color, 1)
+            cv2.rectangle(Screen, (x, y), (x + w, y + h), color, 2)
+
+
 
         if len(rectangles) > 0:
             perks[perk] = len(rectangles)
-            # print(f'{perk} : {len(rectangles)}')
-    # print(perks)
+
+    #double screen size to make it easier to see
+    Screen = cv2.resize(Screen, (Screen.shape[1]*2, Screen.shape[0]*2),interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("Screen", Screen)
+
+
+    # mymin = np.min(entropyList)
+    # min_positions = [i for i, x in enumerate(entropyList) if x == mymin]
+
+    # mymax = np.max(entropyList)
+    # max_positions = [i for i, x in enumerate(entropyList) if x == mymax]
+
     return perks
+
 
 #Issue Perks
 
