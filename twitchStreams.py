182035ccfa063ed62c21127d5b881b9dc9068263
Alex Hey <alexhey1999@gmail.com>
2022-05-04 02:40:13 +0100

    Continued to work on twitch API Intergration

--- twitchStreams.py
+++ twitchStreams.py
@@ -3,6 +3,8 @@ import json
 
 TOKEN_URL = "id.twitch.tv"
 
+URL = "api.twitch.tv"
+
 client_id="f1qxe5nnpqwqu0tvvnzasm3w7qvlkv"
 
 client_secret="4vhg5j8ey6h5ph79080g9jnji0e5al"
@@ -22,9 +24,51 @@ def get_token(client_id, client_secret):
     return token
 
 
+
+def getGameID(clientID,token):
+    conn = http.client.HTTPSConnection(URL)
+    payload = ''
+    headers = {
+    'client-Id': clientID,
+    'Authorization': f'Bearer {token}'
+    }
+    conn.request("GET", "/helix/games?name=Dead%20By%20Daylight", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data)
+    gameID = data["data"][0]["id"]
+    
+    return gameID
+
+
+def getStreams(clientID,token,gameID):
+    conn = http.client.HTTPSConnection(URL)
+    payload = ''
+    headers = {
+    'client-Id': clientID,
+    'Authorization': f'Bearer {token}'
+    }
+    conn.request("GET", f"/helix/streams?game_id={gameID}", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data)
+    listOfStreams = data["data"]
+    return listOfStreams
+
+def getImageOfStream(stream):
+    url = stream["thumbnail_url"]
+    url = url.replace("{width}","1920")
+    url = url.replace("{height}","1080")
+    print(url)
+    return url
+
 def main():
     token = get_token(client_id, client_secret)
-    print(token)
+    gameID = getGameID(client_id,token)
+    streams = getStreams(client_id,token,gameID)
+    
+    for stream in streams:
+        getImageOfStream(stream)
 
 if __name__ == "__main__":
     main()
